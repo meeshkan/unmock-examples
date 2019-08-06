5845db33e4aab7196b0f0202990ea4b73ebad8f8
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-06 13:18:31 +0300

    Add checks for package.json.

--- test-all.sh
+++ test-all.sh
@@ -3,8 +3,13 @@
 set -ex
 
 for directory in */ ; do
+
     echo "Verifying $directory"
     cd $directory
+        if [ ! -f package.json ]; then
+            echo "Skipping $directory"
+            continue
+        fi
         yarn test
     cd ..
 done
--- upgrade-all.sh
+++ upgrade-all.sh
@@ -5,6 +5,10 @@ set -ex
 for directory in */ ; do
     echo "Upgrading $directory"
     cd $directory
+        if [ ! -f package.json ]; then
+            echo "Skipping $directory"
+            continue
+        fi
         yarn
         yarn add unmock-node
     cd ..
