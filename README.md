c439364697335d6baf9df85cd605b18c7a9c5a9f
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-06 13:39:55 +0300

    Add helper scripts for running all examples (#3)
    * Upgrade unmock-versions.
    
    * Add helper scripts.
    
    * Add scripts.
    
    * Add checks for package.json.
    
    * Refactor common code.
    
    * Add run script.
    
    * Replace scripts with one master script.
    
    * Move unmock-node to dev deps for consistency.
    
    * Upgrade unmock-node in dev deps.
    
    * Add upgrade to test.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # unmock-examples
-Simple examples using unmock with 3rd party APIs
+
+Simple examples using [Unmock](https://unmock.io) with 3rd party APIs.
+
+## Scripts
+
+[run.sh](./run.sh) runs scripts in all directories
+
+```bash
+# Run tests in all directories
+./run.sh test
+
+# Upgrade unmock-node in all directories
+./run.sh upgrade
+```
---
+++ run.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -ex
+
+loop_directories () {
+    for directory in */ ; do
+        echo "Upgrading $directory"
+        cd $directory
+            if [ ! -f package.json ]; then
+                echo "Skipping $directory"
+                continue
+            fi
+            $1
+        cd ..
+    done
+}
+
+run_tests () {
+    run_test () {
+        yarn upgrade unmock-node -D
+        yarn test
+    }
+
+    loop_directories run_test
+}
+
+upgrade_unmocks () {
+    upgrade () {
+        yarn
+        yarn add unmock-node -D
+    }
+    loop_directories upgrade
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "One argument expected"
+    exit 1
+fi
+
+if [ "$1" == "test" ]; then
+    run_tests
+elif [ "$1" == "upgrade" ]; then
+    upgrade_unmocks
+else
+    echo "Unknown argument $1"
+    exit 1
+fi
