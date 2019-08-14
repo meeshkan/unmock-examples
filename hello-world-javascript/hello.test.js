07eb5bc6e27fe8512281264dff1725f78dbe9fa5
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-14 11:15:17 +0300

    Add silly expect.

--- hello-world-javascript/hello.test.js
+++ hello-world-javascript/hello.test.js
@@ -1,9 +1,15 @@
 // hello.test.js
 const unmock = require("unmock-node").default;
 const axios = require("axios");
+const jsf = require("json-schema-faker");
 
+console.log("type of jsf.generate", typeof jsf.generate);
 beforeAll(() => {
   unmock.on();
+});
+
+test("jsf.generate type", async () => {
+  expect(typeof jsf.generate).toBe("function");
 });
 
 test("hello endpoint returns correct JSON", async () => {
