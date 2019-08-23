525e73622b88f142aec727da79440db937d4e896
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-08-23 15:19:57 +0300

    Work on petstore repo example.

--- using-service-repository/index.test.ts
+++ using-service-repository/index.test.ts
@@ -1,14 +1,23 @@
-import unmock from "unmock-node";
-import axios from "axios";
+import unmock, { sinon, Service } from "unmock-node";
+import axios, { AxiosResponse } from "axios";
+
+function fetchPets() {
+  return axios("http://petstore.swagger.io/v1/pets").then(
+    (res: AxiosResponse) => res.data
+  );
+}
 
 describe("Petstore", () => {
+  let petstore: Service;
   beforeAll(() => {
-    unmock.on();
+    petstore = unmock.on().services.petstore;
   });
 
   it("uses petstore", async () => {
-    const response = await axios("http://petstore.swagger.io/v1/pets");
-    expect(response.data).toBeDefined();
+    const pets = await fetchPets();
+    sinon.assert.calledOnce(petstore.spy);
+    const mockResponse = petstore.spy.firstCall.returnValue;
+    expect(pets).toHaveLength(JSON.parse(mockResponse.body).length);
   });
   afterAll(() => {
     unmock.off();
