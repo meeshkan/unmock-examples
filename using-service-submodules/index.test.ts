61fd6620227852f9efc6b5a95517d2d5829ce29d
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-15 09:53:41 +0300

    First commit.

---
+++ using-service-submodules/index.test.ts
@@ -0,0 +1,29 @@
+import unmock, { sinon, Service, transform } from "unmock";
+import axios, { AxiosResponse } from "axios";
+const { withCodes } = transform;
+
+function fetchPets() {
+  return axios("http://petstore.swagger.io/v1/pets").then(
+    (res: AxiosResponse) => res.data
+  );
+}
+
+describe("Using unmock with petstore", () => {
+  let petstore: Service;
+  beforeAll(() => {
+    petstore = unmock.on().services.petstore;
+  });
+
+  it("should mock the response with correct structure", async () => {
+    petstore.state(
+      withCodes(200)
+    );
+    const pets = await fetchPets();
+    sinon.assert.calledOnce(petstore.spy);
+    const mockResponse = petstore.spy.firstCall.returnValue;
+    expect(pets).toHaveLength(JSON.parse(mockResponse.body!).length);
+  });
+  afterAll(() => {
+    unmock.off();
+  });
+});
