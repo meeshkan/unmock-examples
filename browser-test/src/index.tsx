a26d6cdc63c800a5fa83b2370db9b53c765eb357
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-12-12 11:40:04 +0200

    Use tilde resolution.

--- browser-test/src/index.tsx
+++ browser-test/src/index.tsx
@@ -1,4 +1,4 @@
-import { fetchFact, fetchJoke } from "./facts";
+import { fetchFact, fetchJoke } from "~/facts";
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
