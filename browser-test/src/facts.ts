9fd7af4efc57f2b7124d04b55a0bf51b9c82659d
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-12-12 14:18:53 +0200

    Use unmock.

--- browser-test/src/facts.ts
+++ browser-test/src/facts.ts
@@ -1,7 +1,20 @@
+import unmock, { u } from "unmock";
+
 const CAT_FACT_URL =
   "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1";
 
 const CN_URL = "http://api.icndb.com/jokes/random";
+
+const unmockOn = () => {
+  const useUnmock = true;
+  unmock.on();
+  unmock
+    .nock("http://api.icndb.com", "chuckNorrisApi")
+    .get("/jokes/random")
+    .reply(200, { value: { joke: u.string("lorem.sentence") } });
+  //.reply(500, "Internal server error");
+  return unmock;
+};
 
 export const fetchFact = async () => {
   const fetchResult = await fetch(CAT_FACT_URL, {
@@ -18,6 +31,7 @@ export const fetchFact = async () => {
 };
 
 export const fetchJoke = async () => {
+  unmockOn();
   const fetchResult = await fetch(CN_URL);
   if (!fetchResult.ok) {
     throw Error(`Failed fetching joke with code: ${fetchResult.status}`);
